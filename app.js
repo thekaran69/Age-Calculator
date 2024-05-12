11a1a802e056db69fcd4332e7dd744aca81db9bb
Karan Singh <119948976+thekaran69@users.noreply.github.com>
2024-05-13 00:08:35 +0530

    updated

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let userInput = document.getElementById("date");
 
+let result = document.getElementById("result");
+
 userInput.max = new Date().toISOString().split("T")[0];
 
 function calculateAge(){
@@ -44,7 +46,7 @@ function calculateAge(){
         y3--;
 
     }
-    console.log(y3,m3,d3);
+    result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months and <span>${d3}</span> days old`;
 
 }
 
